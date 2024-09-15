d47b5b021cb7e97a1bdd54b656501059111425ec
Myo Myat <myomyatmaung28497@gmail.com>
2024-09-15 22:24:33 +0630

    update

--- owl_hotel_booking/__manifest__.py
+++ owl_hotel_booking/__manifest__.py
@@ -7,17 +7,19 @@
         'views/menus.xml',
     ],
     "assets": {
-        "web.assets_backend": [
-        ],
         'owl_hotel_booking.assets_hotel_booking': [
             ('include', 'web._assets_helpers'),
             'web/static/src/scss/pre_variables.scss',
             'web/static/lib/bootstrap/scss/_variables.scss',
             ('include', 'web._assets_bootstrap'),
+            ('include', 'owl_hotel_booking._assets_boostrap5'),
             ('include', 'web._assets_core'),
             'web/static/src/libs/fontawesome/css/font-awesome.css',
             'web/static/lib/odoo_ui_icons/*',
             'owl_hotel_booking/static/src/**/*',
+        ],
+        "owl_hotel_booking._assets_boostrap5": [
+            "owl_hotel_booking/static/lib/bootstrap5/**/*",
         ],
     }
 }
--- owl_hotel_booking/static/src/booking_list/booking_list.js
+++ owl_hotel_booking/static/src/booking_list/booking_list.js
@@ -1,11 +1,15 @@
 /** @odoo-module */
-import {Component,onWillStart,useState} from "@odoo/owl";
+import {Component,onWillStart,useRef,useState} from "@odoo/owl";
 
 export class BookingList extends Component {
     static template = "owl_hotel_booking.BookingList";
     static props = {};
 
     setup(){
-        console.log(this.props)
+        // this.inputRef = useRef("input");
+    }
+
+    _today() {
+        return new Date().toISOString().split("T")[0];
     }
 }
